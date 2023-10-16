d61c439f871db0bdcee36a7a778fc64de73d51c2
ZeroMarker <u78k4l67@163.com>
2023-10-16 11:29:54 +0000

    Add program

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # scheme
 Functional Programming
+
 guile --no-auto-compile -l .init.scm -l main.scm
+
+Chez Scheme
+
+scheme main.scm
+scheme -q main.scm
---
+++ program.scm
@@ -0,0 +1,11 @@
+; my_program.scm
+(define (square x)
+  (* x x))
+
+(display "Enter a number: ")
+(define num (read))
+(display "The square of ")
+(display num)
+(display " is ")
+(display (square num))
+;(newline)
