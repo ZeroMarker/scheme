4dd41ce54208b4c3556516b2f5eee15c56436064
Mark Chen <u78k4l67@163.com>
2024-01-18 14:46:33 +0800

    hanoi tower

--- hanoi.scm
+++ hanoi.scm
@@ -1,12 +1,9 @@
-(define (hanoi n source target aux)
-  (if (= n 0) ; base case - no more disks to move
-      '()
-      (let ((next-move (if (zero? (- n 1))
-                           source
-                           target))) ; recursive call
-        (append (hanoi (- n 1) source next-move aux)
-                (list (cons source target)) ; actual move
-                (hanoi (- n 1) next-move target source)))))
+(define (tower-of-hanoi n source auxiliary target)
+  (if (= n 1)
+      (display (string-append "Move disk from " source " to " target "\n"))
+      (begin
+        (tower-of-hanoi (- n 1) source target auxiliary)
+        (display (string-append "Move disk from " source " to " target "\n"))
+        (tower-of-hanoi (- n 1) auxiliary source target))))
 
-; Example usage: Move 3 disks from peg A to peg B using peg C as an auxiliary peg
-(display (hanoi 3 'a 'b 'c)) ; prints out the moves required to solve the puzzle
+(tower-of-hanoi 3 "A" "B" "C")
